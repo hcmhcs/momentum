6f1369fc1c8fc608a294d60995556091ac0ffe0f
Changmin Han <49056953+hcmhcs@users.noreply.github.com>
2023-01-07 21:34:34 +0900

    Update todo.js

--- js/todo.js
+++ js/todo.js
@@ -3,12 +3,23 @@ const toDoInput = toDoForm.querySelector("input");
 //const toDoInput = document.querySelector("#todo-form input")
 const toDoList = document.getElementById("todo-list");
 
+function deleteTodo(event){
+  const li = event.target.parentElement;
+  li.remove();
+
+}
+
 function paintTodo(newTodo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
   const btn = document.createElement("button");
   li.appendChild(span);
   li.appendChild(btn);
+  span.innerText=newTodo;
+  btn.innerText="x";
+  toDoList.appendChild(li);
+  btn.addEventListener("click",deleteTodo);
+  
 }
 
 function handleToDoSubmit(event) {
@@ -18,3 +29,4 @@ function handleToDoSubmit(event) {
   paintTodo(newTodo);
 }
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
